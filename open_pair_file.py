eb0495ee68b167b78a686005d5efe693f8c26e02
viniputtim <viniputtim@gmail.com>
2025-05-16 18:40:25 -0300

    feat: open paired header/source file on file load

---
+++ open_pair_file.py
@@ -0,0 +1,42 @@
+import os
+import sublime
+import sublime_plugin
+
+
+class MyFileOpenListener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        file_path = view.file_name()
+        file_name = os.path.basename(file_path)
+        file_noext = file_name.split('.')[0]
+        source_exts = ('.c', '.cpp', '.cxx')
+        header_exts = ('.h', '.hpp', '.hxx')
+        sources = ['source', 'src']
+        includes = ['include', 'includes']
+        dirs = file_path.split(os.sep)
+        window = view.window()
+
+        def _open_pair_file(root_folders, pair_folders, pair_exts):
+            indexes = {x: dirs.index(x) for x in root_folders if x in dirs}
+            match = max(indexes, key=indexes.get) if indexes else None
+            match_index = indexes[match] if match else None
+            project_path = os.sep.join(dirs[:match_index]) if match_index else None
+            subdirs_path = os.sep.join(dirs[match_index + 1:-1]) if match_index else None
+
+            if not project_path:
+                return
+
+            include_paths = [os.path.join(project_path, folder) for folder in pair_folders]
+            search_files = [f'{file_noext}{ext}' for ext in pair_exts]
+
+            for include_path in include_paths:
+                for search_file in search_files:
+                    full_path = os.path.join(include_path, subdirs_path, search_file)
+
+                    if os.path.isfile(full_path):
+                        window.open_file(full_path)
+                        return
+
+        if file_name.endswith(source_exts):
+            _open_pair_file(sources, includes, header_exts)
+        elif file_name.endswith(header_exts):
+            _open_pair_file(includes, sources, source_exts)
